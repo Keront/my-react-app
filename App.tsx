02513e743b0ec819519f9b7df155fdd81b389bf8
keront <akill22077@gmail.com>
2025-05-28 19:45:46 +0300

    Message + App

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Message } from './components/Message.tsx';
+
+function App() {
+  const messageText = "Привет, это мое первое React-приложение!";
+  
+  return (
+    <div>
+      <Message text={messageText} />
+    </div>
+  );
+}
+
+export default App;
